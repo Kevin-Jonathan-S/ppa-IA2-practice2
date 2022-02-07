95bd5449301aa649a9f32c0abca8c553fa6aab06
Kevin-Jonathan <94686007+Kevin-Jonathan-S@users.noreply.github.com>
2022-02-07 16:19:45 +0000

    Program 1 and debugging done

--- p1original.c
+++ p1original.c
@@ -1,2 +1,29 @@
+#include<stdio.h>
+#include<math.h>
 
+void input(float *b, float *h)
+{
+  printf("Enter the base of the triangle\n");
+  scanf("%f", b);
+  printf("Enter the height of the triangle\n");
+  scanf("%f", h);
+}
+
+void find_area(float b, float h, float *area)
+{
+  *area= (b*h)/2;
+}
+
+void output(float b, float h, float area)
+{
+  printf(" The area of triangle with base %f and height %f is %f\n",b,h,area);
+}
+
+int main()
+{
+  float b,h,area;
+  input(&b,&h);
+  find_area(b,h,&area);
+  output(b,h,area);
+  return 0;
 }
---
+++ p2original.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<math.h>
+
+void input_side(float *a, float *b, float *c)
+{
+  printf("Enter one side of the triangle\n");
+  scanf("%f", a);
+  printf("Enter the second side of the triangle\n");
+  scanf("%f", b);
+  printf("Enter the third side of the triangle\n");
+  scanf("%f", c)
+}
+
+void check
